f15f0f33fa64b688498e0a34fb42da9af20ab114
mirandaxjill0215 <jbaniqued8@gmail.com>
2026-07-15 18:38:36 -0400

    Add valid login test

---
+++ tests/test_login.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+def test_valid_login():
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com")
+
+    driver.find_element(By.ID, "user-name").send_keys("standard_user")
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    driver.find_element(By.ID, "login-button").click()
+
+    time.sleep(1)
+    assert "inventory" in driver.current_url
+
+    driver.quit()
